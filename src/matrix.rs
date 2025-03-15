36db5a2bee430a2bb2af58272f77ff4d924c62a5
WilgotW <wilgotwardell@hotmail.com>
2025-03-15 21:17:33 +0100

    -

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,5 @@
+use std::ops::{Index, IndexMut};
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Matrix<T> {
     pub rows: usize,
@@ -22,5 +24,23 @@ impl<T: Copy> Matrix<T>{
         //check if matrix dimensions are equal to data amount. if not, panic
         assert_eq!(data.len(), rows * cols);
         Matrix { rows, cols, data }
+    }
+}
+
+impl<T> Index<(usize, usize)> for Matrix<T>{
+    type Output = T;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        let (i, j) = index;
+        assert!(i < self.rows && j > self.cols, "Index out of bounds");
+        &self.data[i * self.cols + j]
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for Matrix<T> {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        let (i, j) = index;
+        assert!(i < self.rows && j < self.cols, "Index out of bounds");
+        &mut self.data[i * self.cols + j]
     }
 }
--- src/ops/matrix_ops.rs
+++ src/ops/matrix_ops.rs
@@ -50,3 +50,42 @@ pub fn mul_scalar <T: std::ops::Mul<Output = T> + Copy>(
 
     return Matrix::from_vec(a.rows, a.cols, result_data)
 }
+
+pub fn dot_product<T: std::ops::Mul<Output = T> + std::ops::Add<Output = T> + Copy + Default>(
+    a: &Matrix<T>,
+    b: &Matrix<T>,
+) -> T {
+    assert_eq!(a.rows, b.rows);
+    assert_eq!(a.cols, b.cols);
+
+    let mut result_data: T = T::default();
+
+    for i in 0..a.data.len() {
+        result_data = result_data + (a.data[i] * b.data[i]);
+    }
+
+    return result_data;
+}
+
+pub fn mat_mul<T: std::ops::Mul<Output = T> + std::ops::Add<Output = T> + Copy>(
+    a: &Matrix<T>,
+    b: &Matrix<T>,
+) -> Matrix<T> {
+    assert_eq!(a.cols, b.rows, "dimension missmatch");
+
+    let mut result_data: Vec<T> = Vec::with_capacity(a.cols * b.rows);
+
+    for i in 0..a.rows {
+        for j in 0..b.cols {
+            let mut sum = a[(i, 0)] * b[(0, j)];
+
+            for k in 1..a.cols {
+                sum = sum + (a[(i, k)] * b[(k, j)]);
+            }
+
+            result_data.push(sum);
+        }
+    }
+
+    return Matrix::from_vec(a.rows, b.cols, result_data);
+}
