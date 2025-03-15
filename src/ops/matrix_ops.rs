0b11a81a1ec1980260adc67e964eb2fc6af2072e
WilgotW <wilgotwardell@hotmail.com>
2025-03-15 20:28:03 +0100

    matrix operations

--- src/ops/matrix_ops.rs
+++ src/ops/matrix_ops.rs
@@ -16,5 +16,37 @@ pub fn add <T: std::ops::Add<Output = T> + Copy>(
         result_data.push(sum);
     }
 
-    Matrix::from_vec(a.rows, a.cols, result_data)
+    return Matrix::from_vec(a.rows, a.cols, result_data)
+}
+
+pub fn sub <T: std::ops::Sub<Output = T> + Copy>(
+    a: &Matrix<T>,
+    b: &Matrix<T>,
+) -> Matrix<T> {
+    assert_eq!(a.rows, b.rows);
+    assert_eq!(a.cols, b.cols);
+
+    let mut result_data: Vec<T> = Vec::with_capacity(a.data.len());
+
+    for i in 0..a.data.len() {
+        let sum = a.data[i] - b.data[i];
+        result_data.push(sum);
+    }
+
+    return Matrix::from_vec(a.rows, a.cols, result_data)
+}
+
+pub fn mul_scalar <T: std::ops::Mul<Output = T> + Copy>(
+    a: &Matrix<T>,
+    scalar: T,
+) -> Matrix<T> {
+
+    let mut result_data: Vec<T> = Vec::with_capacity(a.data.len());
+
+    for i in 0..a.data.len() {
+        let new_val: T = a.data[i] * scalar;
+        result_data.push(new_val);
+    }
+
+    return Matrix::from_vec(a.rows, a.cols, result_data)
 }
